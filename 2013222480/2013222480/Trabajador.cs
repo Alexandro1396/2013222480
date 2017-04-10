3af7df3d8f94cc770e09e236115c6bba438029e2
Alexandro Zavala <chatotextex_333@hotmail.es>
2017-04-09 23:50:46 -0500

    Practica I v1.0

---
+++ 2013222480/2013222480/Trabajador.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2013222480
+{
+     public class Trabajador: TipoTrabajadores
+    {
+        private string _tiptrabajador;
+
+        public string obtenertrabajdor(string _tiptrabajador)
+        {
+        
+
+        }
+
+    }
+}
